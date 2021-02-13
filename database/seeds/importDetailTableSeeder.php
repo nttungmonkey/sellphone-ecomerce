4a4e22faf8eb7c906ea8e4ba12f5fac0afa2d8b9
laogia1234 <anh.tran356@gmail.com>
2021-02-14 05:58:19 +0700

    Thêm dữ liệu nhập hàng

--- database/seeds/importDetailTableSeeder.php
+++ database/seeds/importDetailTableSeeder.php
@@ -11,6 +11,26 @@ class importDetailTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $list = [];
+        
+        $faker = Faker\Factory::create('vi_VN');
+        $pro = DB::table('product')->get();
+        $bill = DB::table('bill_import')->get();
+
+
+        for ($i=0; $i < 80 ; $i++) {
+            $idPro  =   $faker->numberBetween(0,count($pro) - 1);
+            $idBill =   $faker->numberBetween(0, count($bill) - 1);
+            $price  =   $faker->numberBetween(2000,50000);
+            array_push($list, [
+                'imd_price'     => $price,
+                'imd_priceExp'  => $price * $faker->randomFloat(NULL,1.1,2),
+                'imd_amount'    => $faker->numberBetween(2,20),
+                'pro_id'        => $pro[$idPro]->pro_id,
+                'bii_id'        => $bill[$idBill]->bii_id
+            ]);
+        }
+        DB::table('import_detail')->delete();
+        DB::table('import_detail')->insert($list);
     }
 }
